a65da808bc469842137be6c7697a27577f063cdb
ALVIN CODER <tiagosouzasantana24@gmail.com>
2025-09-10 13:36:15 -0300

    index.js

--- index.js
+++ index.js
@@ -39,7 +39,9 @@ app.get("/api/login", async (req, res) => {
     if (useProxy)
       puppeteerArgs.push(`--proxy-server=${proxyHost}:${proxyPort}`);
 
+    // CÓDIGO NOVO E CORRIGIDO
     browser = await puppeteerExtra.launch({
+      executablePath: '/usr/bin/google-chrome', // <-- ADICIONE ESTA LINHA
       headless: true,
       args: [
         "--no-sandbox",
@@ -199,4 +201,3 @@ app.get("/api/login", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
-
